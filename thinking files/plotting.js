4047f2bd0339dbddba98b99c8c53ed74615db811
melissatanja <melissatanja02@users.noreply.github.com>
2018-09-18 20:45:21 -0400

    set up snake "spine"

---
+++ thinking files/plotting.js
@@ -0,0 +1,312 @@
+
+//xpos and zpos have 88 things in them
+var xpos = [];
+var zpos = [];
+var ypos = 50;
+var steps = 10;
+
+var w = 500;
+var h = 500;
+
+function setup(){
+	createCanvas(w, h, WEBGL);
+}
+
+function draw(){
+
+	background(255);
+	orbitControl();
+
+	// noFill();
+	// translate(-200, 0, 250);
+	// beginShape();
+	// vertex(xpos[87], ypos, zpos[86]);
+	// vertex(xpos[86], ypos - 5, zpos[86]);
+	// vertex(xpos[85], ypos, zpos[85] + 4);
+	// vertex(xpos[87], ypos, zpos[86]);
+	// endShape();
+
+	// beginShape();
+	// vertex(xpos[87], ypos, zpos[86]);
+	// vertex(xpos[86], ypos - 5, zpos[86]);
+	// vertex(xpos[85], ypos, zpos[85] - 4);
+	// vertex(xpos[87], ypos, zpos[86]);
+	// endShape();
+
+	// beginShape();
+	// vertex(xpos[87], ypos, zpos[86]);
+	// vertex(xpos[86], ypos + 5, zpos[86]);
+	// vertex(xpos[85], ypos, zpos[85] + 4);
+	// vertex(xpos[87], ypos, zpos[86]);
+	// endShape();
+
+	// beginShape();
+	// vertex(xpos[87], ypos, zpos[86]);
+	// vertex(xpos[86], ypos + 5, zpos[86]);
+	// vertex(xpos[85], ypos, zpos[85] - 4);
+	// vertex(xpos[87], ypos, zpos[86]);
+	// endShape();
+
+	spine();
+
+	triangles();
+
+	// triangle(xpos[30], 50, xpos[31], 50, xpos[30], 40);
+	// rotateY(-PI/10);
+	// rotateY(PI/10);
+
+	// triangle(xpos[30], 50, xpos[31], 50, xpos[30], 40);
+}
+
+function triangles(){
+
+	for(var i = 1; i < 66; i++){
+		if(i < 23){
+
+			noFill();
+			translate(-200, 0, 250);
+			beginShape();
+			vertex(xpos[i], ypos, zpos[i] + (2 * i));
+			vertex(xpos[i + 1], ypos - ((i/2) + 4), zpos[i + 1]);
+			vertex(xpos[i + 2], ypos, zpos[i + 2] + (2 * i));
+			vertex(xpos[i], ypos, zpos[i] + (2 * i));
+			endShape();
+
+			beginShape();
+			vertex(xpos[i], ypos, zpos[i] - (2 * i));
+			vertex(xpos[i + 1], ypos - ((i/2) + 4), zpos[i + 1]);
+			vertex(xpos[i + 2], ypos, zpos[i + 2] - (2 * i));
+			vertex(xpos[i], ypos, zpos[i] - (2 * i));
+			endShape();
+
+			beginShape();
+			vertex(xpos[i], ypos, zpos[i] + (2 * i));
+			vertex(xpos[i + 1], ypos + (((i/2) + 4)/4), zpos[i + 1]);
+			vertex(xpos[i + 2], ypos, zpos[i + 2] + (2 * i));
+			vertex(xpos[i], ypos, zpos[i] + (2 * i));
+			endShape();
+
+			beginShape();
+			vertex(xpos[i], ypos, zpos[i] - (2 * i));
+			vertex(xpos[i + 1], ypos - (((i/2) + 4)/4), zpos[i + 1]);
+			vertex(xpos[i + 2], ypos, zpos[i + 2] - (2 * i));
+			vertex(xpos[i], ypos, zpos[i] - (2 * i));
+			endShape();
+		}
+	}
+}
+
+function spine(){
+
+	var x1 = 10,
+      x2 = 20, 
+      x3 = 30,
+      x4 = 60;
+  var y1 = 50, 
+      y2 = 50, 
+      y3 = 50, 
+      y4 = 50;
+  var z1 = -15, 
+      z2 = -60,
+      z3 = 10,
+      z4 = -25;
+
+  noFill();
+  bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
+
+  for(var i = 0; i <= steps; i++){
+  	var t = i / steps;
+  	var xb = bezierPoint(x1, x2, x3, x4, t);
+  	var zb = bezierPoint(z1, z2, z3, z4, t);
+  	var yb = bezierPoint(y1, y2, y3, y4, t);
+  	translate(xb, yb, zb);
+  	fill(100);
+  	sphere(1);
+  	translate(-xb, -yb, -zb);
+
+  	xpos.push(xb);
+  	zpos.push(zb);
+  }
+ 
+  var x1 = 60,
+      x2 = 70, 
+      x3 = 0,
+      x4 = 45;
+  var z1 = -25, 
+      z2 = -40,
+      z3 = -70,
+      z4 = -90;
+
+  noFill();
+  bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
+
+  for(var i = 0; i <= steps; i++){
+  	var t = i / steps;
+  	var xb = bezierPoint(x1, x2, x3, x4, t);
+  	var zb = bezierPoint(z1, z2, z3, z4, t);
+  	var yb = bezierPoint(y1, y2, y3, y4, t);
+  	translate(xb, yb, zb);
+  	fill(100);
+  	sphere(1);
+  	translate(-xb, -yb, -zb);
+
+  	xpos.push(xb);
+  	zpos.push(zb);
+  }
+  
+  var x1 = 45,
+      x2 = 100, 
+      x3 = 70,
+      x4 = 110;
+  var z1 = -90, 
+      z2 = -115,
+      z3 = -30,
+      z4 = -55;
+
+  noFill();
+  bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
+
+  for(var i = 0; i <= steps; i++){
+  	var t = i / steps;
+  	var xb = bezierPoint(x1, x2, x3, x4, t);
+  	var zb = bezierPoint(z1, z2, z3, z4, t);
+  	var yb = bezierPoint(y1, y2, y3, y4, t);
+  	translate(xb, yb, zb);
+  	fill(100);
+  	sphere(1);
+  	translate(-xb, -yb, -zb);
+
+  	xpos.push(xb);
+  	zpos.push(zb);
+  }
+  
+  var x1 = 110,
+      x2 = 130, 
+      x3 = 80,
+      x4 = 140;
+  var z1 = -55 
+      z2 = -75,
+      z3 = -120,
+      z4 = -110;
+
+  noFill();
+  bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
+
+  for(var i = 0; i <= steps; i++){
+  	var t = i / steps;
+  	var xb = bezierPoint(x1, x2, x3, x4, t);
+  	var zb = bezierPoint(z1, z2, z3, z4, t);
+  	var yb = bezierPoint(y1, y2, y3, y4, t);
+  	translate(xb, yb, zb);
+  	fill(100);
+  	sphere(1);
+  	translate(-xb, -yb, -zb);
+
+  	xpos.push(xb);
+  	zpos.push(zb);
+  }
+  
+  var x1 = 140,
+      x2 = 170, 
+      x3 = 110,
+      x4 = 150;
+  var z1 = -110, 
+      z2 = -90,
+      z3 = -30,
+      z4 = -15;
+
+  noFill();
+  bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
+
+  for(var i = 0; i <= steps; i++){
+  	var t = i / steps;
+  	var xb = bezierPoint(x1, x2, x3, x4, t);
+  	var zb = bezierPoint(z1, z2, z3, z4, t);
+  	var yb = bezierPoint(y1, y2, y3, y4, t);
+  	translate(xb, yb, zb);
+  	fill(100);
+  	sphere(1);
+  	translate(-xb, -yb, -zb);
+
+  	xpos.push(xb);
+  	zpos.push(zb);
+  }
+  
+  var x1 = 150,
+      x2 = 190, 
+      x3 = 140,
+      x4 = 190;
+  var z1 = -15, 
+      z2 = -15,
+      z3 = -90,
+      z4 = -70;
+
+  noFill();
+  bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
+
+  for(var i = 0; i <= steps; i++){
+  	var t = i / steps;
+  	var xb = bezierPoint(x1, x2, x3, x4, t);
+  	var zb = bezierPoint(z1, z2, z3, z4, t);
+  	var yb = bezierPoint(y1, y2, y3, y4, t);
+  	translate(xb, yb, zb);
+  	fill(100);
+  	sphere(1);
+  	translate(-xb, -yb, -zb);
+
+  	xpos.push(xb);
+  	zpos.push(zb);
+  }
+  
+  var x1 = 190,
+      x2 = 230, 
+      x3 = 165,
+      x4 = 190;
+  var z1 = -70,
+      z2 = -50,
+      z3 = -10,
+      z4 = 0;
+
+  noFill();
+  bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
+
+  for(var i = 0; i <= steps; i++){
+  	var t = i / steps;
+  	var xb = bezierPoint(x1, x2, x3, x4, t);
+  	var zb = bezierPoint(z1, z2, z3, z4, t);
+  	var yb = bezierPoint(y1, y2, y3, y4, t);
+  	translate(xb, yb, zb);
+  	fill(100);
+  	sphere(1);
+  	translate(-xb, -yb, -zb);
+
+  	xpos.push(xb);
+  	zpos.push(zb);
+  }
+
+  var x1 = 190,
+      x2 = 210, 
+      x3 = 205,
+      x4 = 230;
+  var z1 = 0,
+      z2 = 0,
+      z3 = -15,
+      z4 = 0;
+
+  noFill();
+  bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
+
+  for(var i = 0; i <= steps; i++){
+  	var t = i / steps;
+  	var xb = bezierPoint(x1, x2, x3, x4, t);
+  	var zb = bezierPoint(z1, z2, z3, z4, t);
+  	var yb = bezierPoint(y1, y2, y3, y4, t);
+  	translate(xb, yb, zb);
+  	fill(100);
+  	sphere(1);
+  	translate(-xb, -yb, -zb);
+
+  	xpos.push(xb);
+  	zpos.push(zb);
+  }
+}
