acb2f3e0311909c4b555ee9fd7620c5a25cea65e
melissatanja <melissatanja02@users.noreply.github.com>
2018-09-20 00:24:45 -0400

    triangles look better, but they don't line up

--- thinking files/snake1.js
+++ thinking files/snake1.js
@@ -5,8 +5,8 @@ var zpos = [];
 var ypos = 50;
 var steps = 5;
 
-var w = 500;
-var h = 500;
+var w = 700;
+var h = 700;
 
 function setup(){
 	createCanvas(w, h, WEBGL);
@@ -18,7 +18,7 @@ function draw(){
 	orbitControl();
 
 	// noFill();
-	translate(-100, 0, 300);
+	translate(-100, 0, 400);
 
 	// for(var i = 5; i < 21; i += 2){
 
@@ -99,15 +99,14 @@ function triangles1(){
 	for(var i = 1; i < 29; i += 2){
 		if(i < 14){
 
-			var sw = i/1.25;
-			var lsw = 11.2;
-			var sh = i/1.5;
-			var lsh = 9.3;
-
 			fill(200, 0, 50);
-			stroke(0, 0, 0, 10);
 
-			if(xpos[i + 1] < xpos[i + 3] + sw || xpos[i + 1] > xpos[i + 3] - sw){
+		var sw = i * 1.25;
+		var lsw = 16.25;
+		var sh = i;
+		var lsh = 13;
+
+		if(xpos[i + 1] < xpos[i + 3] + sw || xpos[i + 1] > xpos[i + 3] - sw){
 				xpos[i + 1] = xpos[i + 1] + sw;
 				xpos[i + 3] = xpos[i + 3] + sw;
 
@@ -119,12 +118,12 @@ function triangles1(){
 				// vertex(xpos[i], ypos, zpos[i] + i/5);
 				endShape();
 
-				beginShape();
-				vertex(xpos[i + 1], ypos, zpos[i + 1] + sw);
-				vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
-				vertex(xpos[i + 3], ypos, zpos[i + 3] + sw);
-				// vertex(xpos[i], ypos, zpos[i] + 16);
-				endShape();
+				// beginShape();
+				// vertex(xpos[i + 1], ypos, zpos[i + 1] + sw);
+				// vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
+				// vertex(xpos[i + 3], ypos, zpos[i + 3] + sw);
+				// // vertex(xpos[i], ypos, zpos[i] + 16);
+				// endShape();
 			} else{
 
 				beginShape();
@@ -134,12 +133,12 @@ function triangles1(){
 				// vertex(xpos[i], ypos, zpos[i] + i/5);
 				endShape();
 
-				beginShape();
-				vertex(xpos[i + 1], ypos, zpos[i + 1] + sw);
-				vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
-				vertex(xpos[i + 3], ypos, zpos[i + 3] + sw);
-				// vertex(xpos[i], ypos, zpos[i] + 16);
-				endShape();
+				// beginShape();
+				// vertex(xpos[i + 1], ypos, zpos[i + 1] + sw);
+				// vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
+				// vertex(xpos[i + 3], ypos, zpos[i + 3] + sw);
+				// // vertex(xpos[i], ypos, zpos[i] + 16);
+				// endShape();
 			}
 
 			if(xpos[i + 1] < xpos[i + 3] + sw || xpos[i + 1] > xpos[i + 3] - sw){
@@ -153,12 +152,12 @@ function triangles1(){
 			// vertex(xpos[i], ypos, zpos[i] - i/5);
 			endShape();
 
-			beginShape();
-			vertex(xpos[i + 1], ypos, zpos[i + 1] - sw);
-			vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
-			vertex(xpos[i + 3], ypos, zpos[i + 3] - sw);
-			// vertex(xpos[i], ypos, zpos[i] - i/5);
-			endShape();
+			// beginShape();
+			// vertex(xpos[i + 1], ypos, zpos[i + 1] - sw);
+			// vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
+			// vertex(xpos[i + 3], ypos, zpos[i + 3] - sw);
+			// // vertex(xpos[i], ypos, zpos[i] - i/5);
+			// endShape();
 
 			}else{
 
@@ -169,12 +168,12 @@ function triangles1(){
 			// vertex(xpos[i], ypos, zpos[i] - i/5);
 			endShape();
 
-			beginShape();
-			vertex(xpos[i + 1], ypos, zpos[i + 1] - sw);
-			vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
-			vertex(xpos[i + 3], ypos, zpos[i + 3] - sw);
-			// vertex(xpos[i], ypos, zpos[i] - i/5);
-			endShape();
+			// beginShape();
+			// vertex(xpos[i + 1], ypos, zpos[i + 1] - sw);
+			// vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
+			// vertex(xpos[i + 3], ypos, zpos[i + 3] - sw);
+			// // vertex(xpos[i], ypos, zpos[i] - i/5);
+			// endShape();
 			}
 
 			// opposite triangles
@@ -191,11 +190,11 @@ function triangles1(){
 			vertex(xpos[i + 2], ypos - sh, zpos[i + 2]);
 			endShape();
 
-			beginShape();
-			vertex(xpos[i], ypos + (sh/4), zpos[i]);
-			vertex(xpos[i + 1], ypos, zpos[i + 1] + sw);
-			vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
-			endShape();			
+			// beginShape();
+			// vertex(xpos[i], ypos + (sh/4), zpos[i]);
+			// vertex(xpos[i + 1], ypos, zpos[i + 1] + sw);
+			// vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
+			// endShape();			
 
 			}else{
 
@@ -205,11 +204,11 @@ function triangles1(){
 			vertex(xpos[i + 2], ypos - sh, zpos[i + 2]);
 			endShape();
 
-			beginShape();
-			vertex(xpos[i], ypos + (sh/4), zpos[i]);
-			vertex(xpos[i + 1], ypos, zpos[i + 1] + sw);
-			vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
-			endShape();	
+			// beginShape();
+			// vertex(xpos[i], ypos + (sh/4), zpos[i]);
+			// vertex(xpos[i + 1], ypos, zpos[i + 1] + sw);
+			// vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
+			// endShape();	
 			}
 
 			if(xpos[i - 1] < xpos[i + 1] + sw || xpos[i - 1] > xpos[i + 1] - sw){
@@ -222,11 +221,11 @@ function triangles1(){
 			vertex(xpos[i + 2], ypos - sh, zpos[i + 2]);
 			endShape();
 
-			beginShape();
-			vertex(xpos[i], ypos + (sh/4), zpos[i]);
-			vertex(xpos[i + 1], ypos, zpos[i + 1] - sw);
-			vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
-			endShape();
+			// beginShape();
+			// vertex(xpos[i], ypos + (sh/4), zpos[i]);
+			// vertex(xpos[i + 1], ypos, zpos[i + 1] - sw);
+			// vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
+			// endShape();
 
 			}else{
 
@@ -236,84 +235,84 @@ function triangles1(){
 			vertex(xpos[i + 2], ypos - sh, zpos[i + 2]);
 			endShape();
 
-			beginShape();
-			vertex(xpos[i], ypos + (sh/4), zpos[i]);
-			vertex(xpos[i + 1], ypos, zpos[i + 1] - sw);
-			vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
-			endShape();
+			// beginShape();
+			// vertex(xpos[i], ypos + (sh/4), zpos[i]);
+			// vertex(xpos[i + 1], ypos, zpos[i + 1] - sw);
+			// vertex(xpos[i + 2], ypos + (sh/4), zpos[i + 2]);
+			// endShape();
 			}
 		}
 
-				if(i >= 14){
+		// 	if(i >= 14){
 
-			fill(200, 0, 50);
-			stroke(0, 0, 0, 10);
+		// 	fill(200, 0, 50);
+		// 	stroke(0, 0, 0, 10);
 
-			beginShape();
-			vertex(xpos[i + 1] + lsw, ypos, zpos[i + 1] + lsw);
-			vertex(xpos[i + 2], ypos - lsh, zpos[i + 2]);
-			vertex(xpos[i + 3] + lsw, ypos, zpos[i + 3] + lsw);
-			// vertex(xpos[i], ypos, zpos[i] + 16);
-			endShape();
+		// 	beginShape();
+		// 	vertex(xpos[i + 1] + lsw, ypos, zpos[i + 1] + lsw);
+		// 	vertex(xpos[i + 2], ypos - lsh, zpos[i + 2]);
+		// 	vertex(xpos[i + 3] + lsw, ypos, zpos[i + 3] + lsw);
+		// 	// vertex(xpos[i], ypos, zpos[i] + 16);
+		// 	endShape();
 
-			beginShape();
-			vertex(xpos[i + 1] - lsw, ypos, zpos[i + 1] - lsw);
-			vertex(xpos[i + 2], ypos - lsh, zpos[i + 2]);
-			vertex(xpos[i + 3] - lsw, ypos, zpos[i + 3] - lsw);
-			// vertex(xpos[i], ypos, zpos[i] - i/5);
-			endShape();
+		// 	beginShape();
+		// 	vertex(xpos[i + 1] - lsw, ypos, zpos[i + 1] - lsw);
+		// 	vertex(xpos[i + 2], ypos - lsh, zpos[i + 2]);
+		// 	vertex(xpos[i + 3] - lsw, ypos, zpos[i + 3] - lsw);
+		// 	// vertex(xpos[i], ypos, zpos[i] - i/5);
+		// 	endShape();
 
-			beginShape();
-			vertex(xpos[i + 1] + lsw, ypos, zpos[i + 1] + lsw);
-			vertex(xpos[i + 2], ypos + 5.4, zpos[i + 2]);
-			vertex(xpos[i + 3] + lsw, ypos, zpos[i + 3] + lsw);
-			// vertex(xpos[i], ypos, zpos[i] + 16);
-			endShape();
+		// 	beginShape();
+		// 	vertex(xpos[i + 1] + lsw, ypos, zpos[i + 1] + lsw);
+		// 	vertex(xpos[i + 2], ypos + 5.4, zpos[i + 2]);
+		// 	vertex(xpos[i + 3] + lsw, ypos, zpos[i + 3] + lsw);
+		// 	// vertex(xpos[i], ypos, zpos[i] + 16);
+		// 	endShape();
 
-			beginShape();
-			vertex(xpos[i + 1] - lsw, ypos, zpos[i + 1] - lsw);
-			vertex(xpos[i + 2], ypos + 5.4, zpos[i + 2]);
-			vertex(xpos[i + 3] - lsw, ypos, zpos[i + 3] - lsw);
-			// vertex(xpos[i], ypos, zpos[i] - i/5);
-			endShape();
+		// 	beginShape();
+		// 	vertex(xpos[i + 1] - lsw, ypos, zpos[i + 1] - lsw);
+		// 	vertex(xpos[i + 2], ypos + 5.4, zpos[i + 2]);
+		// 	vertex(xpos[i + 3] - lsw, ypos, zpos[i + 3] - lsw);
+		// 	// vertex(xpos[i], ypos, zpos[i] - i/5);
+		// 	endShape();
 
-			// opposite triangles
+		// 	// opposite triangles
 
-			fill(80, 0, 10);
+		// 	fill(80, 0, 10);
 
-			beginShape();
-			vertex(xpos[i], ypos - lsh, zpos[i]);
-			vertex(xpos[i + 1] + lsw, ypos, zpos[i + 1] + lsw);
-			vertex(xpos[i + 2], ypos - lsh, zpos[i + 2]);
-			endShape();
+		// 	beginShape();
+		// 	vertex(xpos[i], ypos - lsh, zpos[i]);
+		// 	vertex(xpos[i + 1] + lsw, ypos, zpos[i + 1] + lsw);
+		// 	vertex(xpos[i + 2], ypos - lsh, zpos[i + 2]);
+		// 	endShape();
 
-			beginShape();
-			vertex(xpos[i], ypos - lsh, zpos[i]);
-			vertex(xpos[i + 1] - lsw, ypos, zpos[i + 1] - lsw);
-			vertex(xpos[i + 2], ypos - lsh, zpos[i + 2]);
-			endShape();
+		// 	beginShape();
+		// 	vertex(xpos[i], ypos - lsh, zpos[i]);
+		// 	vertex(xpos[i + 1] - lsw, ypos, zpos[i + 1] - lsw);
+		// 	vertex(xpos[i + 2], ypos - lsh, zpos[i + 2]);
+		// 	endShape();
 
-			beginShape();
-			vertex(xpos[i], ypos + (lsh/4), zpos[i]);
-			vertex(xpos[i + 1] + lsw, ypos, zpos[i + 1] + lsw);
-			vertex(xpos[i + 2], ypos + (lsh/4), zpos[i + 2]);
-			endShape();
+		// 	beginShape();
+		// 	vertex(xpos[i], ypos + (lsh/4), zpos[i]);
+		// 	vertex(xpos[i + 1] + lsw, ypos, zpos[i + 1] + lsw);
+		// 	vertex(xpos[i + 2], ypos + (lsh/4), zpos[i + 2]);
+		// 	endShape();
 
-			beginShape();
-			vertex(xpos[i], ypos + (lsh/4), zpos[i]);
-			vertex(xpos[i + 1] - lsw, ypos, zpos[i + 1] - lsw);
-			vertex(xpos[i + 2], ypos + (lsh/4), zpos[i + 2]);
-			endShape();
-		}
+		// 	beginShape();
+		// 	vertex(xpos[i], ypos + (lsh/4), zpos[i]);
+		// 	vertex(xpos[i + 1] - lsw, ypos, zpos[i + 1] - lsw);
+		// 	vertex(xpos[i + 2], ypos + (lsh/4), zpos[i + 2]);
+		// 	endShape();
+		// }
 	}
 }
 
 function spine(){
 
-	var x1 = 10,
+	var x1 = 0,
       x2 = 20, 
       x3 = 30,
-      x4 = 60;
+      x4 = 50;
   var y1 = 50, 
       y2 = 50, 
       y3 = 50, 
@@ -321,7 +320,7 @@ function spine(){
   var z1 = -15, 
       z2 = -60,
       z3 = 10,
-      z4 = -25;
+      z4 = -15;
 
   // bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
 
@@ -330,19 +329,23 @@ function spine(){
   	var xb = bezierPoint(x1, x2, x3, x4, t);
   	var zb = bezierPoint(z1, z2, z3, z4, t);
   	var yb = bezierPoint(y1, y2, y3, y4, t);
+
+  	// translate(xb, yb, zb);
+  	// sphere(2);
+  	// translate(-xb, -yb, -zb);
 
   	xpos.push(xb);
   	zpos.push(zb);
   }
  
-  var x1 = 60,
+  var x1 = 50,
       x2 = 70, 
-      x3 = 0,
-      x4 = 45;
-  var z1 = -25, 
+      x3 = 20,
+      x4 = 55;
+  var z1 = -15, 
       z2 = -40,
       z3 = -70,
-      z4 = -90;
+      z4 = -100;
 
   // bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
 
@@ -352,15 +355,19 @@ function spine(){
   	var zb = bezierPoint(z1, z2, z3, z4, t);
   	var yb = bezierPoint(y1, y2, y3, y4, t);
 
+  	// translate(xb, yb, zb);
+  	// sphere(2);
+  	// translate(-xb, -yb, -zb);
+
   	xpos.push(xb);
   	zpos.push(zb);
   }
   
-  var x1 = 45,
+  var x1 = 55,
       x2 = 100, 
       x3 = 70,
       x4 = 110;
-  var z1 = -90, 
+  var z1 = -100, 
       z2 = -115,
       z3 = -30,
       z4 = -55;
@@ -373,18 +380,23 @@ function spine(){
   	var zb = bezierPoint(z1, z2, z3, z4, t);
   	var yb = bezierPoint(y1, y2, y3, y4, t);
 
+  	// translate(xb, yb, zb);
+  	// sphere(2);
+  	// translate(-xb, -yb, -zb);
+
   	xpos.push(xb);
   	zpos.push(zb);
   }
   
   var x1 = 110,
       x2 = 130, 
-      x3 = 80,
+      x3 = 100,
       x4 = 140;
   var z1 = -55 
       z2 = -75,
-      z3 = -120,
-      z4 = -110;
+      z3 = -150,
+      z4 = -130;
+
 
   // bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
 
@@ -394,6 +406,10 @@ function spine(){
   	var zb = bezierPoint(z1, z2, z3, z4, t);
   	var yb = bezierPoint(y1, y2, y3, y4, t);
 
+  	// translate(xb, yb, zb);
+  	// sphere(2);
+  	// translate(-xb, -yb, -zb);
+
   	xpos.push(xb);
   	zpos.push(zb);
   }
@@ -402,7 +418,7 @@ function spine(){
       x2 = 170, 
       x3 = 110,
       x4 = 150;
-  var z1 = -110, 
+  var z1 = -130, 
       z2 = -90,
       z3 = -30,
       z4 = -15;
@@ -414,6 +430,10 @@ function spine(){
   	var xb = bezierPoint(x1, x2, x3, x4, t);
   	var zb = bezierPoint(z1, z2, z3, z4, t);
   	var yb = bezierPoint(y1, y2, y3, y4, t);
+
+  	// translate(xb, yb, zb);
+  	// sphere(2);
+  	// translate(-xb, -yb, -zb);
 
   	xpos.push(xb);
   	zpos.push(zb);
@@ -436,18 +456,22 @@ function spine(){
   	var zb = bezierPoint(z1, z2, z3, z4, t);
   	var yb = bezierPoint(y1, y2, y3, y4, t);
 
+  	// translate(xb, yb, zb);
+  	// sphere(2);
+  	// translate(-xb, -yb, -zb);
+
   	xpos.push(xb);
   	zpos.push(zb);
   }
   
   var x1 = 190,
       x2 = 230, 
-      x3 = 165,
-      x4 = 190;
+      x3 = 185,
+      x4 = 200;
   var z1 = -70,
       z2 = -50,
       z3 = -10,
-      z4 = 0;
+      z4 = 10;
 
   // bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
 
@@ -456,19 +480,23 @@ function spine(){
   	var xb = bezierPoint(x1, x2, x3, x4, t);
   	var zb = bezierPoint(z1, z2, z3, z4, t);
   	var yb = bezierPoint(y1, y2, y3, y4, t);
+
+  	// translate(xb, yb, zb);
+  	// sphere(2);
+  	// translate(-xb, -yb, -zb);
 
   	xpos.push(xb);
   	zpos.push(zb);
   }
 
-  var x1 = 190,
-      x2 = 210, 
-      x3 = 205,
-      x4 = 230;
-  var z1 = 0,
+  var x1 = 200,
+      x2 = 230, 
+      x3 = 220,
+      x4 = 240;
+  var z1 = 10,
       z2 = 0,
-      z3 = -15,
-      z4 = 0;
+      z3 = -5,
+      z4 = 10;
 
   // bezier(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4);
 
@@ -477,6 +505,10 @@ function spine(){
   	var xb = bezierPoint(x1, x2, x3, x4, t);
   	var zb = bezierPoint(z1, z2, z3, z4, t);
   	var yb = bezierPoint(y1, y2, y3, y4, t);
+
+  	// translate(xb, yb, zb);
+  	// sphere(2);
+  	// translate(-xb, -yb, -zb);
 
   	xpos.push(xb);
   	zpos.push(zb);
