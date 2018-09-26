778d65702f9e934c2808b5a67626f80f4333136e
melissatanja <melissatanja02@users.noreply.github.com>
2018-09-26 13:28:27 -0400

    snakes circle day sphere

--- thinking files/noisebg.js
+++ thinking files/noisebg.js
@@ -7,7 +7,7 @@ function bg(){
      for (var bgx = 0; bgx < width; bgx++) {
 
        noiseDetail(5,0.5);
-       noiseVal = noise(((frameCount*10)+bgx) * noiseScale,
+       noiseVal = noise(((frameCount*10)-bgx) * noiseScale,
                         (frameCount+bgy) * noiseScale);
        sky.stroke(230 - (noiseVal * 77), 243 - (noiseVal * 37), 255, 150);
        sky.point(bgx,bgy);
